9ccb5d25db7ecc4e58eb6dfc7818ecf9df3231e7
muzuly <matthewbogen06@gmail.com>
2022-12-11 19:59:12 -0600

    About to upgrade

--- scenes/test_interaction.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody
-
-func _ready():
-	self.set_meta("interactable", true)
-
-func interact():
-	$Mesh.get_active_material(0).set_albedo(Color(randf(), randf(), randf()))
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,25 @@
+#Crouching is so so bad
 extends KinematicBody
+
+export var health = 50
 
 export var MOUSE_SENS = 0.08
 export var CAMERA_ACCEL = 40.0
-export var SPEED = 8.0
+export var DEFAULT_SPEED = 8.0
+export var CROUCH_SPEED = 4.0
 export var DEFAULT_ACCEL = 16.0
 export var AIR_ACCEL = 2.0
 export var GRAVITY = 28.0
 export var JUMP_FORCE = 10.0
+export var INERTIA = 1.0
 
 var crosshair_default = load("res://images/hud/crosshair_default.png")
 var crosshair_select = load("res://images/hud/crosshair_select.png")
+var crosshair_moving = load("res://images/hud/crosshair_moving.png")
 
 var ground_cast_contact = false
-var crosshair_is_selection = false
+var is_crouching = false
+var moving = false
 
 var move_direction = Vector3()
 var move_velocity = Vector3()
@@ -22,23 +29,32 @@ var gravity_vector = Vector3()
 var acceleration
 var snap
 var current_velocity
+var selected_interaction
 var camera_target
-var held_object 
+var selected_interactable
+var selected_interactable_class
 
 onready var head = $Head
 onready var camera = head.get_node("Camera")
 onready var camera_ray = camera.get_node("Raycast")
 onready var player_mesh = $Mesh
 onready var crosshair = camera.get_node("Crosshair")
-onready var hold_pos = camera.get_node("HoldPos")
+onready var interactions_list = camera.get_node("Interactions")
+onready var collider = $Collision
 
 func update_crosshair():
-	if (camera_target != null and camera_target.has_meta("interactable") and camera_target.get_meta("interactable")) or (camera_target != null and camera_target.get_class() == "RigidBody"):
-		crosshair.texture = crosshair_select
-		crosshair_is_selection = true
-	elif crosshair_is_selection:
-		crosshair.texture = crosshair_default
-		crosshair_is_selection = false
+	if moving:
+		crosshair.texture = crosshair_moving
+	else:
+		if (camera_target != null and camera_target.has_meta("interactable") and camera_target.get_meta("interactable")) or (camera_target != null and camera_target.get_class() == "RigidBody"):
+			crosshair.texture = crosshair_select
+		else:
+			crosshair.texture = crosshair_default
+		
+func update_health(amount):
+	health += amount 
+	if health > 100:
+		health = 100
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -47,22 +63,57 @@ func _ready():
 
 func _input(event):
 	camera_target = camera_ray.get_collider()
-	update_crosshair()
 	
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(-event.relative.x * MOUSE_SENS))
 		head.rotate_x(deg2rad(-event.relative.y * MOUSE_SENS))
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
 		
-	if Input.is_action_just_pressed("select"):
-		if camera_target != null and camera_target.has_meta("interactable") and camera_target.get_meta("interactable"):
-			camera_target.interact()
+	if Input.is_action_just_pressed("toggle_crouch"):
+		is_crouching = !is_crouching
 		
-	if Input.is_action_just_pressed("pickup"):
-		if camera_target != null and camera_target.get_class() == "RigidBody":
-			held_object = camera_target
-			held_object.mode = RigidBody.MODE_KINEMATIC
-			held_object.collision_mask = 0
+		if is_crouching:
+			collider.shape.height = .5
+			head.translation.y = 1.5
+		else:
+			collider.shape.height = 1.5
+			head.translation.y = 2.5
+	
+	interactions_list.clear()
+	if camera_target != null and not moving and camera_target.has_meta("interactable") and camera_target.get_meta("interactable"):
+		selected_interactable = camera_target
+		var interaction_count = selected_interactable.interactions.size() - 1
+		
+		if  selected_interactable.has_meta("interaction") and selected_interactable_class != selected_interactable.get_meta("interaction"):
+			selected_interaction = 0
+			selected_interactable_class = selected_interactable.get_meta("interaction")
+		
+		var count = 0
+		for interaction in selected_interactable.interactions:
+			interactions_list.add_item(interaction[1])
+			interactions_list.set_item_tooltip_enabled(count, false)
+			count += 1
+			
+		interactions_list.set_item_custom_bg_color(selected_interaction, Color(0.88, 0.88, 0.22, 0.75))
+		interactions_list.visible = true
+		
+		if Input.is_action_just_pressed("select"):
+			selected_interactable.call(selected_interactable.interactions[selected_interaction][0])
+			selected_interactable = null
+			camera_target = null 
+						
+		if Input.is_action_just_pressed("scroll_down"):
+			selected_interaction += 1
+			if selected_interaction > interaction_count:
+				selected_interaction = 0
+		
+		if Input.is_action_just_pressed("scroll_up"):
+			selected_interaction -= 1
+			if selected_interaction < interaction_count - 1:
+				selected_interaction = interaction_count
+	else:
+		selected_interactable = null
+		interactions_list.visible = false
 		
 func _process(delta):
 	#camera physics interpolation black magic to reduce physics jitter on high refresh-rate monitors
@@ -74,8 +125,16 @@ func _process(delta):
 	else:
 		camera.set_as_toplevel(false)
 		camera.global_transform = head.global_transform
+		
+	if current_velocity.x > 5.0 or current_velocity.x < -5.0 or current_velocity.z > 5.0 or current_velocity.z < -5.0:
+		moving = true
+	else:
+		moving = false
+	
+	update_crosshair()
 
 func _physics_process(delta):
+	var speed
 	var horizontal_rotation = global_transform.basis.get_euler().y
 	var forward_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
 	var horizontal_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -95,12 +154,19 @@ func _physics_process(delta):
 		snap = Vector3.ZERO
 		gravity_vector = Vector3.UP * JUMP_FORCE
 	
-	move_velocity = move_velocity.linear_interpolate(move_direction * SPEED, acceleration * delta)
+	if is_crouching:
+		speed = CROUCH_SPEED
+	else:
+		speed = DEFAULT_SPEED
+	
+	move_velocity = move_velocity.linear_interpolate(move_direction * speed, acceleration * delta)
 	movement = move_velocity + gravity_vector
 	
-	current_velocity = move_and_slide_with_snap(movement, snap, Vector3.UP)
+	current_velocity = move_and_slide_with_snap(movement, snap, Vector3.UP, true, 4, PI/4, false)
 	
-	if held_object:
-		held_object.global_transform.origin = hold_pos.global_transform.origin
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider is RigidBody:
+			collision.collider.apply_central_impulse(-collision.normal.normalized() * INERTIA)
 
 
